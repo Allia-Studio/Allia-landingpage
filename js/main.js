bbf2ae41f14f0c300a20b57a058e709ace402679
n4ndp <fernando.adriano.choqque.mejia@gmail.com>
2025-12-23 02:23:51 -0500

    Estructura inicial, navegación terminada y botón contact corregido

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/* ============================================
+   MAIN.JS - Core Functionality
+   ============================================ */
+
+// JavaScript functionality will be added as needed
+
+console.log("Allia Studio - Website Loaded");
